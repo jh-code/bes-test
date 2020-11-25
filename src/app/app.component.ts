dfb111aa163446d6abea68058abc170065a5470b
jh-code <jh.wphax@gmail.com>
2020-11-25 04:46:47 -0600

    switch to using scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './services/data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit  {
   title = 'bes-test';
+
+  constructor(
+    private dataService: DataService
+  ) {
+    //
+  }
+
+  public ngOnInit(): void {
+    //
+  }
 }
